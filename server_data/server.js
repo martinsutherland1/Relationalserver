e0df34ffd81f9bb66e6bb887189ec321d7e4706b
Martin Sutherland <77404854+martinsutherland1@users.noreply.github.com>
2022-05-16 16:23:23 +0100

    end point change

--- server_data/server.js
+++ server_data/server.js
@@ -3,23 +3,25 @@ const app = express();
 
 const cors = require('cors');
 
+const port = process.env.PORT || 5002
+
 app.use(cors());
 app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 
 
-MongoClient.connect('mongodb://localhost:27017')
+MongoClient.connect('mongodb://0.0.0.0:27017')
 .then((client) => {
-  const db = client.db('dataGenerator');
+  const db = client.db('users');
   const dataCreator = db.collection('data');
   const dataRouter = createRouter(dataCreator);
-  app.use('/api/data', dataRouter);
+  app.use('/api/users', dataRouter);
 
   
 })
 .catch(console.err);
 
-app.listen(5002, function () {
+app.listen(port, function () {
   console.log(`Listening on port ${ this.address().port }`);
 });
